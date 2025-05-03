fbdf37a40bef7777684d989530e4c7ad79b4b8d1
ed cuss <edcussmusic@gmail.com>
2025-05-03 09:08:56 +0100

    feat: external src

--- src/cosmic_python_sandbox/fake_io_generation.py
+++ src/cosmic_python_sandbox/fake_io_generation.py
@@ -5,55 +5,76 @@ import isort
 
 
 def generate_fake_io(
-    exts: Sequence[str],
+    db_exts: Sequence[str],
+    external_exts: Sequence[str],
     input_cmd: str = "read",
     output_cmd: str = "write",
     tab: str = "    ",
 ):
-    init_lines = [
-        "import os",
-        "class FakeIO:",
-        f"{tab}def __init__(self, db: dict = None, strict: bool = True):",
-        f"{tab * 2}self.db = db or {{}}",
-        f"{tab * 2}self.strict = strict",
-    ]
-    init = "\n".join(init_lines)
+    init = "\n".join(
+        [
+            "import os",
+            "class FakeIO:",
+            f"{tab}def __init__(self, db: dict = None, external_src: dict = None, strict: bool = True):",
+            f"{tab * 2}self.db = db or {{}}",
+            f"{tab * 2}self.external_src = external_src or {{}}",
+            f"{tab * 2}self.strict = strict",
+        ]
+    )
 
-    check_file_lines = [
-        f"{tab}def _check_file(self, path: str):",
-        f"{tab * 2}if not self.strict:",
-        f"{tab * 3}return\n",
-        f"{tab * 2}if path not in self.db:",
-        f'{tab * 3}raise FileNotFoundError(f"{{path}} not in db {{list(self.db.keys())}}")\n',
-        f"{tab * 2}path_ext = os.path.splitext(path)[-1]",
-        f"{tab * 2}if path_ext:",
-        f"{tab * 3}if not path_ext.endswith(ext):",
-        f'{tab * 4}raise ValueError(f"Path does not have expected ext: {{path = }} {{ext = }}")',
-    ]
-    check_file = "\n".join(check_file_lines)
+    check_file = "\n".join(
+        [
+            f"{tab}def _check_db(self, path: str):",
+            f"{tab * 2}if not self.strict:",
+            f"{tab * 3}return\n",
+            f"{tab * 2}if path not in self.db:",
+            f'{tab * 3}raise FileNotFoundError(f"{{path}} not in db {{list(self.db.keys())}}")\n',
+            f"{tab * 2}path_ext = os.path.splitext(path)[-1]",
+            f"{tab * 2}if path_ext:",
+            f"{tab * 3}if not path_ext.endswith(ext):",
+            f'{tab * 4}raise ValueError(f"Path does not have expected ext: {{path = }} {{ext = }}")',
+        ]
+    )
 
-    read_func_lines = [
-        f"{tab}def _read(self, path: str, ext: str, **kwargs):",
-        f"{tab * 2}self._check_file(path, ext)",
-        f"{tab * 2}return self.db[path]",
-    ]
-    read_func = "\n".join(read_func_lines)
+    read_func = "\n".join(
+        [
+            f"{tab}def _read_db(self, path: str, ext: str, **kwargs):",
+            f"{tab * 2}self._check_db(path, ext)",
+            f"{tab * 2}return self.db[path]",
+        ]
+    )
 
-    write_func_lines = [
-        f"{tab}def _write(self, data, path: str, **kwargs):",
-        f"{tab * 2}self.db[path] = data",
-        f"{tab * 2}return True",
-    ]
-    write_func = "\n".join(write_func_lines)
+    write_func = "\n".join(
+        [
+            f"{tab}def _write_db(self, data, path: str, **kwargs):",
+            f"{tab * 2}self.db[path] = data",
+            f"{tab * 2}return True",
+        ]
+    )
 
-    reset_lines = [f"{tab}def reset(self):", f"{tab * 2}self.db = {{}}"]
-    reset = "\n".join(reset_lines)
+    reset = "\n".join([f"{tab}def reset(self):", f"{tab * 2}self.db = {{}}"])
 
-    funcs = [init, check_file, read_func, write_func, reset]
+    funcs = [init, reset]
 
-    for ext in exts:
+    if db_exts:
+        funcs.extend([check_file, read_func, write_func])
+
+    for ext in db_exts:
         funcs.append(create_input_func(input_cmd, ext, tab))
         funcs.append(create_output_func(output_cmd, ext, tab))
+
+    read_ext_src = "\n".join(
+        [
+            f"{tab}def _read_external_src(self, path: str, **kwargs):",
+            f"{tab * 2}return self.external_src[path]",
+        ]
+    )
+
+    if external_exts:
+        funcs.extend([read_ext_src])
+
+    for src in external_exts:
+        funcs.append(create_ext_input_func(src, tab))
 
     return format_code_str("\n\n".join(funcs))
 
@@ -65,7 +86,7 @@ def format_code_str(code_snippet: str) -> str:
 def create_input_func(input_cmd: str, ext: str, tab: str):
     lines = [
         f"{tab}def {input_cmd}_{ext}(self, path: str, **kwargs):",
-        f"{tab * 2}return self._read(path, '{ext}')",
+        f"{tab * 2}return self._read_db(path, '{ext}')",
     ]
     return "\n".join(lines)
 
@@ -73,7 +94,15 @@ def create_input_func(input_cmd: str, ext: str, tab: str):
 def create_output_func(output_cmd: str, ext: str, tab: str):
     lines = [
         f"{tab}def {output_cmd}_{ext}(self, data, path: str, **kwargs):",
-        f"{tab * 2}return self._write(data, path)",
+        f"{tab * 2}return self._write_db(data, path)",
+    ]
+    return "\n".join(lines)
+
+
+def create_ext_input_func(input_cmd: str, tab: str):
+    lines = [
+        f"{tab}def {input_cmd}(self, path: str, **kwargs):",
+        f"{tab * 2}return self._read_external_src(path)",
     ]
     return "\n".join(lines)
 
