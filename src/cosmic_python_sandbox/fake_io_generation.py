0e9b7194214134957014b5d97e15e0c385d3c15e
ed cuss <edcussmusic@gmail.com>
2025-05-03 18:31:38 +0100

    feat: is_file and list files

--- src/cosmic_python_sandbox/fake_io_generation.py
+++ src/cosmic_python_sandbox/fake_io_generation.py
@@ -17,8 +17,10 @@ def generate_fake_io(
 
     init = (
         "from copy import deepcopy",
+        "from typing import TypeVar",
         "import hashlib",
         "import os",
+        "Data = TypeVar('Data')",
         "class FakeIO:",
         f"{tab}def __init__(self, db: dict = None, external_src: dict = None, strict: bool = True):",
         f"{tab * 2}self.db = db or {{}}",
@@ -35,22 +37,6 @@ def generate_fake_io(
     if standard_ops:
         funcs.extend(get_standard_ops(tab))
 
-    for ext in db_exts:
-        funcs.append(create_input_func(input_cmd, ext, tab))
-        funcs.append(create_output_func(output_cmd, ext, tab))
-
-    read_ext_src = (
-        f"{tab}def _read_external_src(self, path: str, **kwargs):",
-        f"{tab * 2}self.log.append(('external_read', path))",
-        f"{tab * 2}return self.external_src[path]",
-    )
-
-    if external_exts:
-        funcs.extend([read_ext_src])
-
-    for src in external_exts:
-        funcs.append(create_ext_input_func(src, tab))
-
     for hash_type in hash_types:
         funcs.append(
             (
@@ -59,6 +45,22 @@ def generate_fake_io(
                 f"{tab * 2}return hashlib.md5(path.encode()).hexdigest()",
             )
         )
+
+    for ext in db_exts:
+        funcs.append(create_input_func(input_cmd, ext, tab))
+        funcs.append(create_output_func(output_cmd, ext, tab))
+
+    read_ext_src = (
+        f"{tab}def _read_external_src(self, path: str, **kwargs) -> Data:",
+        f"{tab * 2}self.log.append(('external_read', path, kwargs))",
+        f"{tab * 2}return self.external_src[path]",
+    )
+
+    if external_exts:
+        funcs.extend([read_ext_src])
+
+    for src in external_exts:
+        funcs.append(create_ext_input_func(src, tab))
 
     funcs = ("\n".join(lines) for lines in funcs)
 
@@ -71,7 +73,7 @@ def format_code_str(code_snippet: str) -> str:
 
 def create_input_func(input_cmd: str, ext: str, tab: str) -> tuple[str]:
     lines = (
-        f"{tab}def {input_cmd}_{ext}(self, path: str, **kwargs):",
+        f"{tab}def {input_cmd}_{ext}(self, path: str, **kwargs) -> Data:",
         f"{tab * 2}return self._read_db(path, '{ext}')",
     )
     return lines
@@ -79,7 +81,7 @@ def create_input_func(input_cmd: str, ext: str, tab: str) -> tuple[str]:
 
 def create_output_func(output_cmd: str, ext: str, tab: str) -> tuple[str]:
     lines = (
-        f"{tab}def {output_cmd}_{ext}(self, data, path: str, **kwargs):",
+        f"{tab}def {output_cmd}_{ext}(self, data, path: str, **kwargs) -> bool:",
         f"{tab * 2}return self._write_db(data, path)",
     )
     return lines
@@ -87,7 +89,7 @@ def create_output_func(output_cmd: str, ext: str, tab: str) -> tuple[str]:
 
 def create_ext_input_func(input_cmd: str, tab: str) -> tuple[str]:
     lines = (
-        f"{tab}def {input_cmd}(self, path: str, **kwargs):",
+        f"{tab}def {input_cmd}(self, path: str, **kwargs) -> Data:",
         f"{tab * 2}return self._read_external_src(path)",
     )
     return lines
@@ -97,27 +99,27 @@ def get_db_funcs(tab: str) -> tuple[str]:
     reset = (f"{tab}def reset_db(self):", f"{tab * 2}self.db = {{}}")
 
     check_file = (
-        f"{tab}def _check_db(self, path: str):",
+        f"{tab}def _check_db(self, path: str) -> None:",
         f"{tab * 2}if path not in self.db:",
         f'{tab * 3}raise FileNotFoundError(f"{{path = }} not in db {{list(self.db.keys())}}")\n',
         f"{tab * 2}if not self.strict:",
         f"{tab * 3}return\n",
         f"{tab * 2}path_ext = os.path.splitext(path)[-1]",
-        f"{tab * 2}if path_ext:",
+        f"{tab * 2}if path_ext != '':",
         f"{tab * 3}if not path_ext.endswith(ext):",
         f'{tab * 4}raise ValueError(f"Path does not have expected ext: {{path = }} {{ext = }}")',
     )
 
     read_func = (
-        f"{tab}def _read_db(self, path: str, ext: str, **kwargs):",
-        f"{tab * 2}self.log.append(('read', path))",
+        f"{tab}def _read_db(self, path: str, ext: str, **kwargs) -> Data:",
+        f"{tab * 2}self.log.append(('read', path, kwargs))",
         f"{tab * 2}self._check_db(path, ext)",
         f"{tab * 2}return self.db[path]",
     )
 
     write_func = (
-        f"{tab}def _write_db(self, data, path: str, **kwargs):",
-        f"{tab * 2}self.log.append(('write', path))",
+        f"{tab}def _write_db(self, data, path: str, **kwargs) -> bool:",
+        f"{tab * 2}self.log.append(('write', path, kwargs))",
         f"{tab * 2}self.db[path] = data",
         f"{tab * 2}return True",
     )
@@ -129,7 +131,7 @@ def get_standard_ops(tab):
     size_func = (
         f"{tab}def get_size(self, path: str) -> int:",
         f"{tab * 2}self.log.append(('size', path))",
-        f"{tab * 2}return len(path)",
+        f"{tab * 2}return len(path.encode('utf-8'))",
     )
     copy_func = (
         f"{tab}def copy(self, path: str, new_path: str) -> bool:",
@@ -147,12 +149,22 @@ def get_standard_ops(tab):
         f"{tab * 2}return True",
     )
     remove_func = (
-        f"{tab}def remove(self, path: str, new_path: str) -> bool:",
+        f"{tab}def remove(self, path: str) -> bool:",
         f"{tab * 2}self.log.append(('remove', path))",
         f"{tab * 2}self.db.pop(path)",
         f"{tab * 2}return True",
     )
-    return size_func, copy_func, move_func, remove_func
+    exists_func = (
+        f"{tab}def is_file(self, path: str) -> bool:",
+        f"{tab * 2}self.log.append(('is_file', path))",
+        f"{tab * 2}return path in self.db",
+    )
+    list_files_func = (
+        f"{tab}def list_files(self, prefix: str = '') -> list[str]:",
+        f"{tab * 2}self.log.append(('list_files', path))",
+        f"{tab * 2}return [p for p in self.db if p.startswith(prefix)]",
+    )
+    return size_func, copy_func, move_func, remove_func, exists_func, list_files_func
 
 
 def write_str(data: str, path: str) -> Literal[True]:
