5bd29cd98ef2b3bbe9082ec8600178819f295eb8
ed cuss <edcussmusic@gmail.com>
2025-05-31 19:39:21 +0100

    feat: pl adapter

--- src/cosmic_python_sandbox/adapters/io_wrappers/pl_io.py
+++ src/cosmic_python_sandbox/adapters/io_wrappers/pl_io.py
@@ -1,11 +1,11 @@
 import attrs
-import pandas as pd
+import polars as pl
 
 from cosmic_python_sandbox.adapters.io_wrappers._io_protocol import Data, FileType
 
 
 @attrs.define
-class PandasIO:
+class PolarsIO:
     def setup(self) -> bool:
         return True
 
@@ -15,11 +15,11 @@ class PandasIO:
     def read(self, path: str, file_type: FileType, **kwargs: dict) -> Data:
         match file_type:
             case FileType.CSV:
-                return pd.read_csv(path, **kwargs)
+                return pl.read_csv(path, **kwargs)
             case FileType.JSON:
-                return pd.read_json(path, **kwargs)
+                return pl.read_json(path, **kwargs)
             case FileType.PARQUET:
-                return pd.read_parquet(path, **kwargs)
+                return pl.read_parquet(path, **kwargs)
             case _:
                 raise NotImplementedError(f"`{file_type}` is not implemented")
         return True
@@ -27,11 +27,11 @@ class PandasIO:
     def write(self, path: str, data: Data, file_type: FileType, **kwargs: dict) -> bool:
         match file_type:
             case FileType.CSV:
-                data.to_csv(path, index=False, **kwargs)
+                data.write_csv(path, **kwargs)
             case FileType.JSON:
-                data.to_json(path, index=False, orient="records", **kwargs)
+                data.write_json(path, **kwargs)
             case FileType.PARQUET:
-                data.to_parquet(path, index=False, **kwargs)
+                data.write_parquet(path, **kwargs)
             case _:
                 raise NotImplementedError(f"`{file_type}` is not implemented")
         return True
