5eac137483ceaa79db9d0d27b46accbb63e2ce29
ed cuss <edcussmusic@gmail.com>
2025-07-16 20:41:56 +0100

    feat: monad result types

--- src/cosmic_python_sandbox/usecases/_event.py
+++ src/cosmic_python_sandbox/usecases/_event.py
@@ -1,6 +1,13 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import attrs
 
-from cosmic_python_sandbox.service_layer.uow import UnitOfWorkProtocol
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from cosmic_python_sandbox.service_layer.uow import UnitOfWorkProtocol
 
 
 @attrs.define
@@ -8,7 +15,7 @@ class Event:
     priority_event: bool = attrs.field(default=False)
 
     @classmethod
-    def from_dict(cls, msg: dict) -> "Event":
+    def from_dict(cls, msg: dict) -> Event:
         filtered = {f.name: msg[f.name] for f in attrs.fields(cls) if f.name in msg}
         return cls(**filtered)
 
@@ -17,3 +24,31 @@ def example_handler(event: Event, uow: UnitOfWorkProtocol) -> None:
     with uow:
         err_msg = f"Must use a specialised event. Given {event}"
         raise ValueError(err_msg)
+
+
+@attrs.define
+class Ok:
+    event: Event | Sequence[Event] | None = attrs.field(
+        default=None,
+        validator=attrs.validators.optional(
+            attrs.validators.or_(
+                attrs.validators.instance_of(Event),
+                attrs.validators.deep_iterable(attrs.validators.instance_of(Event)),
+            ),
+        ),
+    )
+
+
+@attrs.define
+class Err:
+    event: Event = attrs.field(validator=attrs.validators.instance_of(Event))
+    error: Exception = attrs.field(validator=attrs.validators.instance_of(Exception))
+    err_type: BaseException = attrs.field(init=False)
+    err_msg: str = attrs.field(init=False)
+    traceback: dict = attrs.field(init=False)
+
+    def __attrs_post_init__(self) -> None:
+        self.err_type = type(self.error)
+        self.err_msg = str(self.error)
+        self.traceback = self.error.__traceback__.tb_frame.f_locals
+        self.traceback["line_no"] = self.error.__traceback__.tb_lineno
