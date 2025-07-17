4204505a4cabd85571cde4e49674d47d2923bc4b
ed cuss <edcussmusic@gmail.com>
2025-07-17 18:57:17 +0100

    fix: add decorator to other handlers

--- src/cosmic_python_sandbox/usecases/delete_file.py
+++ src/cosmic_python_sandbox/usecases/delete_file.py
@@ -2,7 +2,7 @@ import attrs
 from attrs.validators import min_len
 
 from cosmic_python_sandbox.service_layer.uow import UnitOfWorkProtocol
-from cosmic_python_sandbox.usecases._event import Event
+from cosmic_python_sandbox.usecases._event import Event, catch_err
 
 
 @attrs.define
@@ -10,6 +10,7 @@ class DeleteFile(Event):
     dst: str = attrs.field(default="", validator=[min_len(3)])
 
 
+@catch_err
 def delete_file(event: DeleteFile, uow: UnitOfWorkProtocol) -> None:
     with uow:
         uow.logger.info({"guid": uow.guid, "event": event})
